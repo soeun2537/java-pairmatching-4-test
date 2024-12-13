928752e380f8778d71fb47d28e39d79b30b56f26
soeun2537 <soeun2537@gmail.com>
2024-12-13 14:01:11 +0900

    feat: Application 설정

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,15 @@
 package pairmatching;
 
+import pairmatching.controller.PairMatchingController;
+import pairmatching.repository.MatchingRepository;
+import pairmatching.service.PairMatchingService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        MatchingRepository matchingRepository = new MatchingRepository();
+        matchingRepository.initialize();
+        PairMatchingService pairMatchingService = new PairMatchingService(matchingRepository);
+        PairMatchingController pairMatchingController = new PairMatchingController(pairMatchingService);
+        pairMatchingController.run();
     }
 }
